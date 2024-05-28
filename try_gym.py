54674340b4ee297a9099038b38601f98172e9365
Kamran <kamran.vat@protonmail.com>
2024-05-28 21:41:05 +0200

    trying out three input neurons

--- try_gym.py
+++ try_gym.py
@@ -13,39 +13,53 @@ import numpy as np
 #   Box(-2.0, 2.0, (1,), float32)
 
 def observation_to_freq(observation, min_obs, max_obs, min_freq=1, max_freq=100):
-    # normalize the observation
+    # don't think this function is needed actually
     observation = (observation - min_obs) / (max_obs - min_obs)
-    # convert to  (frequency)
     freq = min_freq + (max_freq - min_freq) * observation
 
 def normalize_obs(observation, min_obs, max_obs):
     """return normalized observation between 0 and 1 (for freq encoding)"""
     return (observation - min_obs) / (max_obs - min_obs)
 
-def normalize_obs_space(observation):
-    """normalize each value of the observation space with its respective high and low"""
-    return [normalize_obs(observation[i], observation.low[i], observation.high[i]) for i in range(len(observation))]
+# if we use this with frequency encoding, we will need to use 3 input neurons firing at 50% to represent 0 (0% for -1, 100% for 1), 
+#   instead of 6 input neurons which actually go from 0% for 0 to 100% for 1. 
+def normalize_obs_space(observation, min_obs, max_obs):
+    """normalize each value of the observation space with its respective high and low
+    
+    Args:
+        observation (np.ndarray): observation space
+
+    Returns:
+        list: normalized observation space
+    """
+    return [normalize_obs(observation[i], min_obs[i], max_obs[i]) for i in range(len(observation))]
 
 def generate_spike(odds):
     """generate a spike with the given odds (0-1)"""
     return np.random.rand() < odds
 
 
-env = gym.make('Pendulum-v1', render_mode="human", g=1) # default is g = 10
+env = gym.make('Pendulum-v1', render_mode="human", g=1) # default is g = 10, earth gravity is 9.8
 observation, info = env.reset()
-
-# print the observation space, and for each value in it print the min and max
-print(env.observation_space)
-print(env.observation_space.low)
-print(env.observation_space.high)
-
-# idea for how to convert to spikes:
+min_obs = env.observation_space.low
+max_obs = env.observation_space.high
 
 
-
+# try stuff out - three input neurons:
 for _ in range(1000):
     action = env.action_space.sample()  # agent policy that uses the observation and info
     observation, reward, terminated, truncated, info = env.step(action)
+
+    # represent the neurons as colors in the pygame window, light up when a spike is generated
+
+    # normalize the action space
+    observation = normalize_obs_space(observation, min_obs, max_obs)
+
+    # generate spikes for the input neurons
+    spikes = [generate_spike(neuron) for neuron in observation]
+    print(spikes)
+
+
 
     if terminated or truncated:
         observation, info = env.reset()
