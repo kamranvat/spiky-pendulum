4f85d2a1435df0db7865553cd15a6ad5b6be1c96
Kamran <kamran.vat@protonmail.com>
2024-05-29 11:22:18 +0200

    added missing signed values fct

--- try_gym.py
+++ try_gym.py
@@ -15,15 +15,14 @@ from gymnasium.wrappers import TransformObservation
 #   Box(-2.0, 2.0, (1,), float32)
 
 
-def observation_to_freq(observation, min_obs, max_obs, min_freq=1, max_freq=100):
-    # don't think this function is needed actually
-    observation = (observation - min_obs) / (max_obs - min_obs)
-    freq = min_freq + (max_freq - min_freq) * observation
-
-
 def normalize_observation(observation, min_obs, max_obs):
     """return normalized observation between 0 and 1 (for freq encoding)"""
     return (observation - min_obs) / (max_obs - min_obs)
+
+
+def normalize_observation_signed(observation, min_obs, max_obs):
+    """return normalized observation between -1 and 1 (for freq encoding)"""
+    return 2 * (observation - min_obs) / (max_obs - min_obs) - 1
 
 
 def normalize_observation_absolute(observation, min_obs, max_obs):
@@ -32,7 +31,7 @@ def normalize_observation_absolute(observation, min_obs, max_obs):
     (i.e. for the observation [0.1, -0.2, 0.3], return [0.1, 0, 0, 0.2, 0.3, 0]).
     Also normalize the values between 0 and 1.
     """
-    observation = normalize_observation(observation, min_obs, max_obs)
+    observation = normalize_observation_signed(observation, min_obs, max_obs)
     abs_observation = []
     for value in observation:
         if value >= 0:
@@ -49,27 +48,23 @@ def generate_spike(odds):
     return np.random.rand() < odds
 
 
-env = gym.make(
-    "Pendulum-v1", render_mode="human", g=1
-)  # default is g = 10, earth gravity is 9.8
+# Generate environment:
+env = gym.make("Pendulum-v1", render_mode="human", g=1)  # default: g=10
 min_obs = env.observation_space.low
 max_obs = env.observation_space.high
 env = TransformObservation(
-    env, lambda obs: normalize_observation(obs, min_obs, max_obs)
+    env, lambda obs: normalize_observation_absolute(obs, min_obs, max_obs)
 )
-normalize_observation_absolute([0.1, -0.2, 0.3], min_obs, max_obs)
 
 observation, info = env.reset()
 # try stuff out - three input neurons:
 for _ in range(1000):
-    action = (
-        env.action_space.sample()
-    )  # agent policy that uses the observation and info
+    action = env.action_space.sample()  # random policy
     observation, reward, terminated, truncated, info = env.step(action)
 
     # generate spikes for the input neurons
     spikes = [generate_spike(neuron) for neuron in observation]
-    print(observation[2])
+    print(observation)
 
     if terminated or truncated:
         observation, info = env.reset()
