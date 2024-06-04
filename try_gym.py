6a6eec48c33af3d7f22fbc3437d65673a49c6611
kamranvat <kamran.vat@gmail.com>
2024-06-04 12:50:43 +0200

    added random output spikes -> action conversion, included output in demo print

--- try_gym.py
+++ try_gym.py
@@ -27,9 +27,9 @@ def normalize_observation_signed(observation, min_obs, max_obs):
 
 def normalize_observation_absolute(observation, min_obs, max_obs):
     """
-    for each value in the observation, return two values, one for the positive and one for the negative value
-    (i.e. for the observation [0.1, -0.2, 0.3], return [0.1, 0, 0, 0.2, 0.3, 0]).
-    Also normalize the values between 0 and 1.
+    for each value in the observation, generate two values, one for the positive and one for the negative absolute value
+    (i.e. for the observation [0.1, -0.2, 0.3], convert to [0.1, 0,   0, 0.2,   0.3, 0]).
+    Then normalize the values between 0 and 1 and return the result.
     """
     observation = normalize_observation_signed(observation, min_obs, max_obs)
     abs_observation = []
@@ -42,16 +42,36 @@ def normalize_observation_absolute(observation, min_obs, max_obs):
     return abs_observation
 
 
+def spikes_to_action(spikes, min_action, max_action):
+    """convert output of two spiking neurons to action space"""
+    # we could add more fine-grained control with more neurons or by using membrane voltage
+    if spikes[0] and not spikes[1]:
+        action = min_action
+    elif not spikes[0] and spikes[1]:
+        action = max_action
+    else:
+        action = np.zeros_like(min_action)
+
+    return action
+
+
 def generate_spike(odds: float) -> bool:
-    # TODO: check if the rate is good - maybe reduce odds in here
-    # didn't we do that with a poisson process instead?
+    # TODO: check if this is the right way to generate spikes
+    # didn't we do that with a poisson process instead? -> yes we did
     """generate a spike with the given odds (0-1)"""
     return np.random.rand() < odds
 
 
-def pretty_print_spikes(spikes):
+def pretty_print_spikes(input_spikes, output_spikes):
     """print the spikes in a human readable way"""
-    print("".join([" X " if spike else "   " for spike in spikes]))
+    print(
+        ("  |     ")
+        + "".join([" X " if spike else "   " for spike in input_spikes])
+        + ("     |     ")
+        + ("").join([" <- " if output_spikes[0] else "    ", " -> " if output_spikes[1] else "    "])
+        + ("     |  ")
+    )
+
 
 
 # Generate environment:
@@ -67,14 +87,18 @@ env = TransformObservation(
 )
 
 observation, info = env.reset()
-# try stuff out - three input neurons:
+
+# try stuff out - for six input neurons:
 for _ in range(1000):
     action = env.action_space.sample()  # random policy
+    # generate random spikes of two output neurons
+    output_spikes = [generate_spike(0.5), generate_spike(0.5)]
+    action = spikes_to_action(output_spikes, env.action_space.low, env.action_space.high)
     observation, reward, terminated, truncated, info = env.step(action)
 
-    # generate spikes for the input neurons
-    spikes = [generate_spike(neuron) for neuron in observation]
-    pretty_print_spikes(spikes)
+    # generate spikes for the input neurons (for current timestep)
+    input_spikes = [generate_spike(neuron) for neuron in observation]
+    pretty_print_spikes(input_spikes, output_spikes)
 
     if terminated or truncated:
         observation, info = env.reset()
