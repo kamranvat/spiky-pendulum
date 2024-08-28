ddbcdbc81b2254d87a364a51c89d1be12fa3fb4d
MeinChef <yapa.nilop@gmail.com>
2024-08-28 14:03:17 +0200

    removed gauss and sigmoid

--- encoders/reward_shaping.py
+++ encoders/reward_shaping.py
@@ -5,15 +5,15 @@ MAX_REWARD = np.square(np.pi) + 0.1 * np.square(8) + 0.001 * np.square(2)
 
 def bin_reward(r):
     if r < -12:
-        return 0
+        return -0.5
     elif r < -8:
-        return 0.25
+        return -0.25
     elif r < -4:
-        return 0.5
+        return 0
     elif r < 0:
-        return 0.75
+        return 0.25
     elif r >= 0:
-        return 1
+        return 0.5
 
 def norm_reward_shift(r):
     return r + MAX_REWARD / 2
